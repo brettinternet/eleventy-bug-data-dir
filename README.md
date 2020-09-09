7cdd1a1d9a76a2834283ff6e87087fe0e04fc267
brett <brettinternet@gmail.com>
2020-09-08 18:06:17 -0600

    add documentation links, add notes about issue and resolution

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Eleventy Bug
 
 This demonstrates a potential Eleventy bug where JS data files are not correctly handled when the `data` folder is above the `input` scope.
+
+### Update
+
+It looks like this issue only occurs when `input` receives an absolute path.
--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,18 @@
 const path = require("path");
 
-module.exports = (_config) => {
-  return {
-    dir: {
-      input: path.resolve("src"),
-      output: path.resolve("build"),
-      data: "../data", // must be a relative path to `input`
-    },
-  };
+module.exports = {
+  dir: {
+    /**
+     * @docs https://www.11ty.dev/docs/config/#input-directory
+     * @note it looks like this value _must_ be relative for
+     * JS data files to be imported correctly
+     */
+    // input: "src", // Works!
+    input: path.resolve("src"), // Doesn't work!
+    /**
+     * @docs https://www.11ty.dev/docs/config/#directory-for-global-data-files
+     * @note must be a relative path to `input`
+     */
+    data: "../data",
+  },
 };
